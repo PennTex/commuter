857955cf5111192f2ee32ed1217025fe9e05c74c
marioharper <harpmari12@gmail.com>
2016-10-07 13:22:40 -0500

    Updates list short desc

--- cmd/list.go
+++ cmd/list.go
@@ -9,7 +9,7 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "Get a list of your current available locations",
+	Short: "List saved location",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO: Work your own magic here
