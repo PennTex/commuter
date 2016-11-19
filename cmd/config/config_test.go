a1b6003eb263735dc3df7f117e21d505b98b7aac
Mario Harper <harpmari12@gmail.com>
2016-11-19 11:31:12 -0600

    Adds more config tests

--- cmd/config/config_test.go
+++ cmd/config/config_test.go
@@ -1,15 +1,147 @@
 package config_test
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/marioharper/commuter/cmd/config"
+	"github.com/marioharper/commuter/directions"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConfig_New(t *testing.T) {
-	configFile := "_fixtures/config.json"
-	theConfig := config.New(configFile)
+var configFile = "_fixtures/temp_config.json"
 
+func setup(configMock config.Config) {
+	// create test config file
+	f, err := os.Create(configFile)
+	if err != nil {
+		fmt.Printf("creating config file: %s \n", err.Error())
+		os.Exit(-1)
+	}
+
+	// convert config to json
+	configJSON, err := json.MarshalIndent(configMock, "", "  ")
+	if err != nil {
+		fmt.Printf("marshalling config file: %s \n", err.Error())
+		os.Exit(-1)
+	}
+
+	// write json to config file
+	_, err = f.WriteString(string(configJSON))
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	f.Sync()
+	f.Close()
+}
+
+func TestConfig_New(t *testing.T) {
+	var locations = []directions.Location{
+		directions.Location{
+			Name:    "work",
+			Address: "1600 Amphitheatre Pkwy, Mountain View, CA 94043",
+		},
+		directions.Location{
+			Name:    "home",
+			Address: "1060 North Rengstorff Avenue, Mountain View, CA 94043",
+		},
+	}
+	var configMock = config.Config{
+		Locations: locations,
+	}
+	setup(configMock)
+
+	theConfig := config.New(configFile)
 	assert.Equal(t, theConfig.File, configFile)
+}
+
+func TestConfig_GetLocations(t *testing.T) {
+	var locations = []directions.Location{
+		directions.Location{
+			Name:    "work",
+			Address: "1600 Amphitheatre Pkwy, Mountain View, CA 94043",
+		},
+		directions.Location{
+			Name:    "home",
+			Address: "1060 North Rengstorff Avenue, Mountain View, CA 94043",
+		},
+	}
+	var configMock = config.Config{
+		Locations: locations,
+	}
+	setup(configMock)
+
+	theConfig := config.New(configFile)
+	assert.Equal(t, theConfig.GetLocations(), locations)
+}
+
+func TestConfig_GetLocationByName(t *testing.T) {
+	var locations = []directions.Location{
+		directions.Location{
+			Name:    "work",
+			Address: "1600 Amphitheatre Pkwy, Mountain View, CA 94043",
+		},
+		directions.Location{
+			Name:    "home",
+			Address: "1060 North Rengstorff Avenue, Mountain View, CA 94043",
+		},
+	}
+	var configMock = config.Config{
+		Locations: locations,
+	}
+	setup(configMock)
+
+	theConfig := config.New(configFile)
+	retrievedLocation, err := theConfig.GetLocationByName("work")
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, retrievedLocation, locations[0])
+}
+
+func TestConfig_DeleteLocation(t *testing.T) {
+	var locations = []directions.Location{
+		directions.Location{
+			Name:    "work",
+			Address: "1600 Amphitheatre Pkwy, Mountain View, CA 94043",
+		},
+		directions.Location{
+			Name:    "home",
+			Address: "1060 North Rengstorff Avenue, Mountain View, CA 94043",
+		},
+	}
+	var configMock = config.Config{
+		Locations: locations,
+	}
+	setup(configMock)
+
+	theConfig := config.New(configFile)
+	theConfig.DeleteLocation("work")
+	expectedLocations := []directions.Location{
+		directions.Location{
+			Name:    "home",
+			Address: "1060 North Rengstorff Avenue, Mountain View, CA 94043",
+		},
+	}
+	assert.Equal(t, theConfig.GetLocations(), expectedLocations)
+}
+
+func TestConfig_AddLocation(t *testing.T) {
+	var locations = []directions.Location{}
+	var configMock = config.Config{
+		Locations: locations,
+	}
+	setup(configMock)
+
+	theConfig := config.New(configFile)
+	newLocation := directions.Location{
+		Name:    "new",
+		Address: "new location",
+	}
+
+	theConfig.AddLocation(newLocation)
+	assert.Equal(t, theConfig.Config.Locations[0], newLocation)
 }
--- cmd/config/main.go
+++ cmd/config/main.go
@@ -8,50 +8,50 @@ import (
 	"github.com/marioharper/commuter/directions"
 )
 
-type config struct {
+type Config struct {
 	Locations []directions.Location
 }
 
 type ConfigManager struct {
 	File string
-	config
+	Config
 }
 
 func New(file string) ConfigManager {
-	return ConfigManager{File: file, config: getConfig(file)}
+	return ConfigManager{File: file, Config: getConfig(file)}
 }
 
 func (cm *ConfigManager) DeleteLocation(locationName string) {
 
-	locationIdx := getLocationIdxByName(cm.config.Locations, locationName)
+	locationIdx := getLocationIdxByName(cm.Config.Locations, locationName)
 
-	cm.config.Locations[locationIdx] = cm.config.Locations[len(cm.config.Locations)-1]
-	cm.config.Locations[len(cm.config.Locations)-1] = directions.Location{}
-	cm.config.Locations = cm.config.Locations[:len(cm.config.Locations)-1]
+	cm.Config.Locations[locationIdx] = cm.Config.Locations[len(cm.Config.Locations)-1]
+	cm.Config.Locations[len(cm.Config.Locations)-1] = directions.Location{}
+	cm.Config.Locations = cm.Config.Locations[:len(cm.Config.Locations)-1]
 
 	cm.saveConfig()
 }
 
 func (cm *ConfigManager) AddLocation(location directions.Location) {
 
-	if i := getLocationIdxByName(cm.config.Locations, location.Name); i >= 0 {
+	if i := getLocationIdxByName(cm.Config.Locations, location.Name); i >= 0 {
 		fmt.Println("You already have a location with the same name")
 		os.Exit(-1)
 		return
 	}
 
-	cm.config.Locations = append(cm.config.Locations, location)
+	cm.Config.Locations = append(cm.Config.Locations, location)
 
 	cm.saveConfig()
 }
 
 func (cm *ConfigManager) GetLocations() []directions.Location {
-	return cm.config.Locations
+	return cm.Config.Locations
 }
 
 func (cm *ConfigManager) GetLocationByName(name string) (directions.Location, error) {
 
-	for _, location := range cm.config.Locations {
+	for _, location := range cm.Config.Locations {
 		if location.Name == name {
 			return location, nil
 		}
@@ -70,8 +70,8 @@ func getLocationIdxByName(locations []directions.Location, name string) int {
 	return -1
 }
 
-func getConfig(configFile string) config {
-	var theConfig config
+func getConfig(configFile string) Config {
+	var theConfig Config
 	var f *os.File
 	var err error
 
@@ -111,7 +111,7 @@ func (cm *ConfigManager) saveConfig() {
 	}
 
 	// convert config to json
-	configJSON, err := json.MarshalIndent(cm.config, "", "  ")
+	configJSON, err := json.MarshalIndent(cm.Config, "", "  ")
 	if err != nil {
 		fmt.Printf("marshalling config file: %s \n", err.Error())
 		os.Exit(-1)
